4d162088bcd20e13903475690b20937ef2768dfb
ChrisJonathan22 <christophereko@hotmail.fr>
2018-03-09 21:13:40 +0000

    Make a Deal
    First Commit.

---
+++ js/script.js
@@ -0,0 +1,46 @@
+//Blue door code
+
+/*
+
+var blueDoor = document.getElementById("image1");
+blueDoor.addEventListener("click", change, false);
+
+function change() {
+   window.location = "http://127.0.0.1:5500/Make%20a%20Deal/choose1.html";
+   if(window.location == "http://127.0.0.1:5500/Make%20a%20Deal/choose1.html"){
+       location.replace("choose1final.html")
+        orangeDoor.removeEventListener("click", change2);
+   }
+   
+}
+
+
+//Orange door code
+var orangeDoor = document.getElementById("image2");
+orangeDoor.addEventListener("click", change2, false);
+
+function change2() {
+    window.location = "http://127.0.0.1:5500/Make%20a%20Deal/choose2.html";
+
+    if (window.location == "http://127.0.0.1:5500/Make%20a%20Deal/choose2.html") {
+        location.replace("choose2final.html");
+    }
+}
+
+//Green door code
+var greenDoor = document.getElementById("image3");
+greenDoor.addEventListener("click", change3, false);
+
+
+function change3() {
+    window.location = "http://127.0.0.1:5500/Make%20a%20Deal/choose3.html";
+
+    if (window.location == "http://127.0.0.1:5500/Make%20a%20Deal/choose3.html") {
+        location.replace("choose3final.html");
+        
+    }
+    
+}
+
+
+*/
